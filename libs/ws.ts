c7df27ee7ee58aeec6e4a7287cab7e76b3c61647
gengjiawen <technicalcute@gmail.com>
2020-05-05 20:42:40 +0800

    misc fix

--- libs/ws.ts
+++ libs/ws.ts
@@ -3,11 +3,11 @@ import * as Websocket from 'ws'
 const ws = new Websocket('ws://echo.websocket.org')
 
 ws.on('open', function open() {
-   console.log('open')
-   ws.send('h2')
+  console.log('open')
+  ws.send('h2')
 })
 
 ws.on('message', function incoming(data) {
-    console.log(data)
-    ws.close()
+  console.log(data)
+  ws.close()
 })
