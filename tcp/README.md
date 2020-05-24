a7b0b96675cd7fdf33001fd40b06a16dd1adff99
gengjiawen <technicalcute@gmail.com>
2020-05-24 22:49:56 +0800

    add tcp rust part 1

---
+++ tcp/README.md
@@ -0,0 +1,9 @@
+# A progressive tcp implementation
+
+Only work on linux.
+
+By vidoe: https://www.youtube.com/watch?v=bzja9fQWzdA
+
+RFC 793 — https://tools.ietf.org/html/rfc793
+
+Original source at https://github.com/jonhoo/rust-tcp
---
+++ tcp/src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+use tun_tap;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8;1504];
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    }
+    Ok(())
+}
